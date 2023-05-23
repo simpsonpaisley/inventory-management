eb49d0cdaea57e0e86052da5de8e01d8d4ea0515
simpsonpaisley <simpsonpaisley@gmail.com>
2023-05-23 18:50:11 +0100

    Removed unused variables

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,27 +1,27 @@
-import background from '../images/background.svg';
-
 function SignIn() {
 	return (
 		<div className="signIn">
-			<h2>Log In</h2>
-			<form>
-				<label htmlFor="email">Email:</label>
-				<input
-					type="email"
-					placeholder="johndoe@example.com"
-					id="email"
-					value="email"></input>
-				<label htmlFor="password">Password:</label>
-				<input
-					type="password"
-					id="password"
-					value="password"></input>
-				<button
-					type="submit"
-					className="logIn">
-					Log In
-				</button>
-			</form>
+			<div className="signInForm">
+				<h2>Log In</h2>
+				<form>
+					<label htmlFor="email">Email:</label>
+					<input
+						type="email"
+						placeholder="johndoe@example.com"
+						id="email"
+						required></input>
+					<label htmlFor="password">Password:</label>
+					<input
+						type="password"
+						id="password"
+						required></input>
+					<button
+						type="submit"
+						className="logIn">
+						Log In
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 }
