c7c2b407b62215d251a4ab1685447da65400482c
simpsonpaisley <simpsonpaisley@gmail.com>
2023-05-23 19:28:16 +0100

    Changed routing

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,6 +1,6 @@
+import SignIn from '../components/SignIn';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import SignIn from '../components/SignIn';
 
 function LogIn() {
 	return (
